38beda1c54d06176f44647d2a2d3b9ec134d197f
Nakanishi Tetsuhiro <kaoru6strings@gmail.com>
2016-12-02 14:35:53 +0900

    Update q7p35.py

--- q7_p35.py
+++ q7_p35.py
@@ -1,6 +1,15 @@
+# Q. 以下の手順を踏んだ時、元の日付と同じになる日にちを答えよ。期間は1964/10/10から2020/07/24
+#
+# 1. 日付のフォーマットはYYYYMMDD
+# 2. 2進数に変換
+# 3. 2進数を逆から並べる
+# 4. 逆から並べた2進数を10進数に戻す
+
+
 from datetime import date, timedelta
 
 
+# dateオブジェクトを投げるとrangeしてくれるすごいやつ
 def daterange(start_date, end_date):
 
     for n in range((end_date - start_date).days):
@@ -9,14 +18,15 @@ def daterange(start_date, end_date):
 
 if __name__ == '__main__':
 
-    start = date(1964, 10, 10)
-    end = date(2020, 7, 24)
+    start   = date(1964, 10, 10)
+    end     = date(2020, 7, 24)
     results = []
     
     for i in daterange(start, end):
         tmp_date = int(i.strftime("%Y%m%d")) # ８桁のフォーマットに合わせる
-        tmp_date_binnum_reverse = format(tmp_date, 'b')[::-1]
-        tmp_date_binnum_reverse_decimalnum = int(tmp_date_binnum_reverse, 2)
+        tmp_date_binnum_reverse = format(tmp_date, 'b')[::-1] # 2進数にしてReverse
+        tmp_date_binnum_reverse_decimalnum = int(tmp_date_binnum_reverse, 2) # 10進数に変換
+
         if tmp_date == tmp_date_binnum_reverse_decimalnum:
             results.append(tmp_date)
 
